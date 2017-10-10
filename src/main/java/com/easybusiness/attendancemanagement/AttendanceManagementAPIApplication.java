83df93ccdba4b95dd2618109d4a409d5002e5a16
singhmanish3007 <singhmanish3007@gmail.com>
2017-10-10 21:39:19 +0530

    changed to war from jar

--- src/main/java/com/easybusiness/attendancemanagement/AttendanceManagementAPIApplication.java
+++ src/main/java/com/easybusiness/attendancemanagement/AttendanceManagementAPIApplication.java
@@ -5,25 +5,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.easybusiness.attendancemanagement", "com.easybusiness.attendancepersistence"})
+@ComponentScan(basePackages = { "com.easybusiness.attendancemanagement", "com.easybusiness.attendancepersistence" })
 @EnableAutoConfiguration
-public class AttendanceManagementAPIApplication implements CommandLineRunner {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(AttendanceManagementAPIApplication.class);
+public class AttendanceManagementAPIApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AttendanceManagementAPIApplication.class, args);
-		LOGGER.info("started AttendanceManagementAPIApplication");
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(AttendanceManagementAPIApplication.class);
 
-	@Override
-	public void run(String... args) throws Exception {
-	    LOGGER.info("in run method");
-	    
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	return builder.sources(AttendanceManagementAPIApplication.class);
+    }
+
+    public static void main(String[] args) {
+	SpringApplication.run(AttendanceManagementAPIApplication.class, args);
+	LOGGER.info("started AttendanceManagementAPIApplication");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+	LOGGER.info("in run method");
+
+    }
 
 }
